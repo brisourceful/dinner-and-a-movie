fa39a26bbeded5112fbdf243d0b6ba394ba09020
Logan Absher <absherlogan@gmail.com>
2017-04-03 14:09:54 -0700

    routes.js added

---
+++ public/scripts/controllers/routes.js
@@ -0,0 +1,5 @@
+'use strict';
+page('/', );
+page();
+page();
+page();
---
+++ public/scripts/controllers/searchController.js
@@ -0,0 +1,7 @@
+'use strict';
+(function(module){
+  const searchController = {};
+  searchController.initSearch = function(){
+
+  }
+})(window);
